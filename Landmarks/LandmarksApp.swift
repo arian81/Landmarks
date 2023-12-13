835e49d0bbe05f6a875b1d95f872acc3aa6f23f7
Arian Ahmadinejad <ahmadinejadarian@gmail.com>
2023-12-12 21:21:24 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Arian Ahmadinejad on 2023-12-12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
